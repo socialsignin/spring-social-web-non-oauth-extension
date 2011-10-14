02b63e46342389a27f21a5d3b3eb993f4f385333
Michael Lavelle <michael@lavelle.name>
2011-10-14 02:03:21 +0100

    Modified groupId in pom to reflect change move from socialauth to socialsignin repository, added javadoc and licenses

--- src/main/java/org/springframework/social/connect/web/ExtensibleProviderSignInController.java
+++ src/main/java/org/springframework/social/connect/web/ExtensibleProviderSignInController.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.social.connect.web;
 
 import java.util.List;
@@ -15,6 +30,16 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
+/**
+* Modified version of ProviderSignInController which can be subclassed
+* so as to allow use of non-OAuth1 and OAuth2 Providers (eg. Last.fm) whose 
+* authentication dance is similar to OAuth but has some differences.
+* ProviderSignInController itself has restriction of either OAuth1 or OAuth2
+* 
+* TODO - Fork ProviderSignInController in spring-social-web
+* 
+* @author Michael Lavelle
+*/
 public class ExtensibleProviderSignInController extends
 		ProviderSignInController {
 
